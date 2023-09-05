8b7d9f749c50d7fea88ad4967a291493560707b0
Henry <john_henry.4m@hotmail.com>
2023-09-05 16:56:30 -0500

    clase 5 septiembre.

--- src/ApuntesJava/ListaDeListas2.java
+++ src/ApuntesJava/ListaDeListas2.java
@@ -7,26 +7,28 @@ import java.util.Scanner;
 
 public class ListaDeListas2 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
         List<List<String>> usuarios = new ArrayList<>();
-        ArrayList<String> nuevousuario1 = new ArrayList<>();
-        ArrayList<String> nuevousuario2 = new ArrayList<>();
-        ArrayList<String> nuevousuario3 = new ArrayList<>();
-        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> usuario1 = new ArrayList<>();
+        ArrayList<String> usuario2 = new ArrayList<>();
+        ArrayList<String> usuario3 = new ArrayList<>();
 
-        nuevousuario1.add("nombre");
-        nuevousuario2.add("edad");
-        nuevousuario3.add("correo");
-        for(int i = 0; i< nuevousuario1.size(); i ++) {
-            System.out.println(nuevousuario1.get(1));
+        usuario3.add("nombre");
+        usuario3.add("edad");
+        usuario3.add("correo");
+        for (int i=0; i<3; i++){
+            System.out.println("ingrese un dato");
+            String dato= sc.nextLine();
+            usuario3.add(dato);
         }
+        for ( int i = 0; i < 3 ; i ++){
+            List<String> nuevaLista = new ArrayList<>();
+            nuevaLista.add(("Nombre" + (i + 1)));
+            nuevaLista.add(("Edad" + (i + 1)));
+            nuevaLista.add(("Correo" + (i + 1)));
+            usuarios.add(nuevaLista);
 
-
-
-
-
-
-
-
+        }
     }
 }
